aceeb83c70516a4ee7ea606299bd02c298080c71
Sören Balko <soerenbalko@microsoft.com>
2024-06-07 09:02:28 +1000

    Remove more redundant bits

--- src/shared/controllable-promise.ts
+++ src/shared/controllable-promise.ts
@@ -1,5 +1,3 @@
-type SynchronousPromiseStatus = 'pending' | 'resolved' | 'rejected';
-
 export class ControllablePromise<T> {
     get promise(): Promise<T> {
         return this._promise;
@@ -17,16 +15,12 @@ export class ControllablePromise<T> {
     private _reject!: (reason?: Error) => void;
     private _resolve!: (value: T) => void;
 
-    private _status: SynchronousPromiseStatus = 'pending';
-
     constructor() {
         this._promise = new Promise((resolve, reject) => {
             this._resolve = value => {
-                this._status = 'resolved';
                 resolve(value);
             };
             this._reject = error => {
-                this._status = 'rejected';
                 reject(error);
             };
         });
